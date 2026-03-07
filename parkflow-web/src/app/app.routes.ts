2e88b1fecec114544a49d068776c932df93f8231
impalea <thamicoiado.26@gmail.com>
2026-03-07 19:08:41 -0300

    adjusting html app

--- parkflow-web/src/app/app.routes.ts
+++ parkflow-web/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './components/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: 'dashboard', component: Dashboard },
+	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+];
