ea32802961ca7c9c3e417f6d1d9d33abd13f72b6
impalea <thamicoiado.26@gmail.com>
2026-03-13 16:02:49 -0300

    creating checkin and checkout logic

--- parkflow-web/src/app/components/dashboard/dashboard.ts
+++ parkflow-web/src/app/components/dashboard/dashboard.ts
@@ -1,40 +1,136 @@
-import { Component, OnInit, signal } from '@angular/core';
-import { ParkingSpot, ParkingSpotDashboard } from '../../services/parking-spot';
+import { Component, computed, OnInit, signal } from '@angular/core';
+import { ParkingSpot, ParkingSpotDashboard } from '../../services/parking-spot/parking-spot';
+import { CheckOutPreview, Ticket } from '../../services/ticket/ticket';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-dashboard',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, FormsModule],
 	templateUrl: './dashboard.html',
 	styleUrl: './dashboard.scss',
 })
 export class Dashboard implements OnInit {
 	spots = signal<ParkingSpotDashboard[]>([]);
 
-	constructor(private parkingSpotService: ParkingSpot) { }
+	totalSpots = computed(() => this.spots().length);
+
+	totalOccupied = computed(() =>
+  	this.spots().filter(s => s.isOccupied).length
+	);
+
+	totalAvailable = computed(() =>
+  	this.spots().filter(s => !s.isOccupied).length
+	);
+
+	selectedSpot = signal<ParkingSpotDashboard | null>(null);
+	checkOutData = signal<CheckOutPreview | null>(null);
+
+	isCheckInModalOpen = signal(false);
+	isCheckOutModalOpen = signal(false);
+
+	currentTicketId = 0;
+	checkInForm = {
+		licensePlate: '',
+		model: '',
+		color: '',
+		parkingSpotId: 0
+	}
+
+	constructor(
+		private parkingSpotService: ParkingSpot,
+		private ticketService: Ticket
+	) { }
 
 	ngOnInit(): void {
+		this.loadDashboard();
+	}
+
+	loadDashboard(): void {
 		this.parkingSpotService.getDashboard().subscribe({
 			next: (data) => {
 				this.spots.set(data);
 			},
-			error: (error) => {
-				console.error('Error fetching parking spot dashboard:', error);
+			error: (err) => {
+				console.error('Error fetching parking spot dashboard:', err);
 			}
 		});
 	}
 
-	doCheckIn(spot: ParkingSpotDashboard) {
-		console.log('Iniciando Check-in para a vaga:', spot.spotNumber);
-		// No futuro, aqui abrirá um modal ou formulário
-		alert(`Abrir formulário de entrada para a vaga ${spot.spotNumber}`);
+	doCheckIn(spot: ParkingSpotDashboard): void {
+		this.selectedSpot.set(spot);
+		this.isCheckInModalOpen.set(true);
+	}
+
+	createCheckIn() {
+		const spot = this.selectedSpot();
+		if (!spot) return;
+
+		const cleanPlate = this.checkInForm.licensePlate
+		.toUpperCase()
+		.replace(/[^A-Z0-9]/g, '');
+
+		const payload = {
+			licensePlate: cleanPlate,
+			model: this.checkInForm.model,
+			color: this.checkInForm.color,
+			parkingSpotId: spot.parkingSpotId
+		};
+
+		this.ticketService.createCheckIn(payload).subscribe({
+			next: () => {
+				this.closeCheckInModal();
+				this.loadDashboard();
+			},
+			error: (err) => {
+				console.error('Error during check-in:', err);
+				alert('Falha ao registrar entrada.');
+			}
+		});
 	}
 
 	doCheckOut(spot: ParkingSpotDashboard) {
-		console.log('Iniciando Check-out para o ticket:', spot.ticketId);
-		// No futuro, aqui abrirá um modal ou formulário
-		alert(`Confirmar saída do veículo ${spot.licensePlate}`);
+		if (!spot.ticketId) return;
+
+		this.currentTicketId = spot.ticketId;
+
+		this.ticketService.getCheckOutPreview(spot.ticketId).subscribe({
+			next: (data) => {
+				this.checkOutData.set(data);
+				this.isCheckOutModalOpen.set(true);
+			},
+			error: (err: any) => {
+				console.error('Error fetching check-out preview:', err);
+			}
+		});
+	}
+
+	confirmCheckOut() {
+		if (!this.currentTicketId) return;
+
+		this.ticketService.confirmCheckOut(this.currentTicketId).subscribe({
+			next: () => {
+				this.isCheckOutModalOpen.set(false);
+				this.loadDashboard();
+			},
+			error: (err: any) => {
+				console.error('Error confirming check-out:', err);
+				alert('Falha ao finalizar check-out.')
+			}
+		});
+	}
+
+	closeCheckInModal() {
+		this.isCheckInModalOpen.set(false);
+		this.selectedSpot.set(null);
+		this.checkInForm = { licensePlate: '', model: '', color: '', parkingSpotId: 0 };
+	}
+
+	closeCheckoutModal() {
+		this.isCheckOutModalOpen.set(false);
+		this.checkOutData.set(null);
+		this.currentTicketId = 0;
 	}
 }
 
