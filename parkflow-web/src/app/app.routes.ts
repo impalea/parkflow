e40bb4362cdce18b43f0c5ad6c8b706ee093cf4c
impalea <thamicoiado.26@gmail.com>
2026-04-11 14:32:55 -0300

    adding new settings page to the app header to improve navigation

--- parkflow-web/src/app/app.routes.ts
+++ parkflow-web/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './components/dashboard/dashboard';
+import { PriceConfig } from './components/price-config/price-config';
 
 export const routes: Routes = [
 	{ path: 'dashboard', component: Dashboard },
+	{ path: 'settings', component: PriceConfig },
 	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
--- parkflow-web/src/app/app.ts
+++ parkflow-web/src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+	selector: 'app-root',
+	imports: [RouterOutlet],
+	templateUrl: './app.html',
+	styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('parkflow-web');
+	protected readonly title = signal('parkflow-web');
+
+	constructor(private router: Router) {}
+
+	goToSettings() {
+		this.router.navigate(['/settings']);
+	}
+
+	goToHome() {
+		this.router.navigate(['/']);
+	}
 }
