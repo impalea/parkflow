47658943aacdad7863ef63fff4590188abacde09
impalea <thamicoiado.26@gmail.com>
2026-05-08 23:05:38 -0300

    consuming new parking spot apis

--- parkflow-web/src/app/services/parking-spot/parking-spot.ts
+++ parkflow-web/src/app/services/parking-spot/parking-spot.ts
@@ -14,15 +14,33 @@ export interface ParkingSpotDashboard {
 	entryTime?: string;
 }
 
+export interface Spot {
+	id: number;
+	spotNumber: string;
+	isOccupied: boolean;
+}
+
 @Injectable({
 	providedIn: 'root'
 })
-export class ParkingSpot {
+export class ParkingSpotService {
 	private apiUrl = environment.apiUrl + '/api/parkingspot';
 
 	constructor(private http: HttpClient) { }
 
 	getDashboard(): Observable<ParkingSpotDashboard[]> {
 		return this.http.get<ParkingSpotDashboard[]>(`${this.apiUrl}/dashboard`);
+	}
+
+	getAll(): Observable<Spot[]> {
+		return this.http.get<Spot[]>(this.apiUrl);
+	}
+
+	create(spot: { spotNumber: string }): Observable<Spot> {
+		return this.http.post<Spot>(this.apiUrl, spot);
+	}
+
+	delete(id: number): Observable<any> {
+		return this.http.delete<any>(`${this.apiUrl}/${id}`);
 	}
 }
