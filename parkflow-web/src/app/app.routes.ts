6c7cd6db034b477e0c343e038901b27e24f2feba
impalea <thamicoiado.26@gmail.com>
2026-05-08 23:07:12 -0300

    fixing routes

--- parkflow-web/src/app/app.routes.ts
+++ parkflow-web/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './components/dashboard/dashboard';
 import { PriceConfig } from './components/price-config/price-config';
+import { ParkingSpot } from './components/parking-spot/parking-spot';
+import { Settings } from './components/settings/settings';
 
 export const routes: Routes = [
 	{ path: 'dashboard', component: Dashboard },
-	{ path: 'settings', component: PriceConfig },
+	{ path: 'settings', component: Settings },
+	{ path: 'settings/prices', component: PriceConfig },
+	{ path: 'settings/parking-spots', component: ParkingSpot },
 	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
